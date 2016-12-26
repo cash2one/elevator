17650edbbda06d2188dd700a674717ce992c22b4
diggzhang <diggzhang@gmail.com>
2016-12-26 17:29:31 +0800

    Add events view stuff

--- controllers/events.js
+++ controllers/events.js
@@ -3,10 +3,7 @@ const views = require('co-views');
 const parse = require('co-body');
 const messages = [
   { id: 0,
-    message: 'Elevator'
-  },
-  { id: 1,
-    message: 'The monitor for production events'
+    message: 'Elevator: the monitor for production events'
   }
 ];
 
@@ -16,4 +13,16 @@ const render = views(__dirname + '/../views', {
 
 module.exports.home = function *home(ctx) {
   this.body = yield render('list', { 'messages': messages });
+};
+
+module.exports.dailyProductionEvents = function *dailyProductionEvents(ctx) {
+  //this.body = yield render('dp', { 'messages': messages });
+  var data = JSON.parse(
+    require('fs').readFileSync(
+      require('path').resolve(
+        __dirname, 'data/eventV4DailyProductionEventsCount.json'
+      ),
+      'utf8'
+    ));
+  this.body = data;
 };
---
+++ public/scripts/daily-events-production.js
@@ -0,0 +1,57 @@
+$.getJSON('http://localhost:3000/daily',function(data){
+  var dataLength = data.length;
+  console.log(dataLength);
+  var Frame = G2.Frame;
+  var frame = new Frame(data);
+  frame = Frame.combinColumns(frame, ['app', 'backend','total'], 'value', 'event type', 'date');
+  var chart = new G2.Chart({
+    id: 'c0',
+    width : 1000,
+    height : 500,
+    plotCfg: {
+      margin: [20, 120, 80, 80]
+    }
+  });
+  chart.setMode('select'); // 开启框选模式
+  chart.select('rangeX'); // 选择框选交互形式
+  chart.source(frame, {
+    date: {
+      type: 'time',
+      mask: 'yyyy.mm.dd',
+      tickCount: dataLength
+    },
+    value: {
+      alias: '日生产埋点量级监控'
+    }
+  });
+  chart.axis('date', {
+    line: null,
+    tickLine: {
+      stroke: '#000',
+      value: 6 // 刻度线长度
+    },
+    title: null
+  });
+  chart.axis('value', {
+    tickLine: {
+      stroke: '#000',
+      value: 6 // 刻度线长度
+    },
+    labels: {
+      label: {
+        fill: '#000'
+      }
+    },
+    line: {
+      stroke: '#000'
+    },
+    grid: null
+  });
+  chart.line().position('date*value').color('event type', ['#1f77b4', '#ff7f0e', '#2ca02c']).shape('spline').size(2);
+  chart.render();
+
+  chart.on('plotdbclick', function(ev) {
+    chart.set('filters', {});
+    chart.repaint();
+  });
+});
